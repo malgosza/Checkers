b15d202e895e600cb7c6997dc465ebfe03bc6254
Gosia <malgosza.niewiadomska@gmail.com>
2020-04-30 10:09:40 +0200

    Filling the tab

--- src/main/java/StartApp.java
+++ src/main/java/StartApp.java
@@ -1,2 +1,38 @@
 public class StartApp {
+    public static void main(String[] args) {
+        plansza();
+    }
+
+    public static void plansza() {
+        String[] plansza = new String[64];
+        int licznik = 0;
+
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                plansza[i] = "C";
+            } else {
+                plansza[i] = "O";
+            }
+        }
+
+        for (int i = 8; i < plansza.length; i++) {
+            if (licznik % 8 == 0) {
+                licznik = 0;
+                if (plansza[i - 1].equals("C")) {
+                    plansza[i] = "C";
+                    licznik++;
+                } else {
+                    plansza[i] = "O";
+                    licznik++;
+                }
+            } else {
+                licznik++;
+                if (plansza[i - 1].equals("C")) {
+                    plansza[i] = "O";
+                } else {
+                    plansza[i] = "C";
+                }
+            }
+        }
+    }
 }
