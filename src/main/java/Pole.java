4125d15ec01801b94515475769c8d214a77d951c
Gosia <malgosza.niewiadomska@gmail.com>
2020-05-04 12:20:45 +0200

    Adding Pole class

---
+++ src/main/java/Pole.java
@@ -0,0 +1,12 @@
+public class Pole {
+
+    int number;
+    String color;
+    String fieldContent;
+
+    public Pole(int number, String color, String fieldContent){
+        this.number=number;
+        this.color=color;
+        this.fieldContent=fieldContent;
+    }
+}
--- src/main/java/StartApp.java
+++ src/main/java/StartApp.java
@@ -1,41 +1,93 @@
+import java.util.Scanner;
+
 public class StartApp {
     public static void main(String[] args) {
-        plansza();
+        ruchGracza();
+
     }
 
-    public static void plansza() {
+    public static String[] plansza() {
         String[] plansza = new String[64];
         int licznik = 0;
 
         for (int i = 0; i < 8; i++) {
             if (i % 2 == 0) {
-                plansza[i] = "O";
+                plansza[i] = "o";
             } else {
-                plansza[i] = "C";
+                plansza[i] = "Y";
             }
         }
 
-        for (int i = 8; i < plansza.length; i++) {
+        for (int i = 8; i <24; i++) {
             if (licznik % 8 == 0) {
                 licznik = 0;
-                if (plansza[i - 1].equals("O")) {
-                    plansza[i] = "O";
+                if (plansza[i - 1].equals("o")) {
+                    plansza[i] = "o";
                     licznik++;
                 } else {
-                    plansza[i] = "C";
+                    plansza[i] = "Y";
                     licznik++;
                 }
             } else {
                 licznik++;
-                if (plansza[i - 1].equals("O")) {
-                    plansza[i] = "C";
+                if (plansza[i - 1].equals("o")) {
+                    plansza[i] = "Y";
                 } else {
-                    plansza[i] = "O";
+                    plansza[i] = "o";
                 }
             }
         }
-        
-        displayPlansza(plansza);
+///////-----------------------------------
+        for (int i = 24; i < 40; i++) {
+            if (i % 2 == 0) {
+                plansza[i] = "o";
+            } else {
+                plansza[i] = "c";
+            }
+        }
+
+        ///-----------------------------
+        licznik = 0;
+        for (int i = 40; i < 48; i++) {
+            if (licznik % 8 == 0) {
+                licznik = 0;
+                if (plansza[i - 1].equals("o")) {
+                    plansza[i] = "o";
+                    licznik++;
+                } else {
+                    plansza[i] = "X";
+                    licznik++;
+                }
+            } else {
+                licznik++;
+                if (plansza[i - 1].equals("o")) {
+                    plansza[i] = "X";
+                } else {
+                    plansza[i] = "o";
+                }
+            }
+        }
+
+        for (int i = 40; i < 64; i++) {
+            if (licznik % 8 == 0) {
+                licznik = 0;
+                if (plansza[i - 1].equals("o")) {
+                    plansza[i] = "o";
+                    licznik++;
+                } else {
+                    plansza[i] = "X";
+                    licznik++;
+                }
+            } else {
+                licznik++;
+                if (plansza[i - 1].equals("o")) {
+                    plansza[i] = "X";
+                } else {
+                    plansza[i] = "o";
+                }
+            }
+        }
+        return plansza;
     }
 
     public static void displayPlansza(String[] plansza) {
@@ -44,6 +96,30 @@ public class StartApp {
                 System.out.println();
             }
             System.out.print(plansza[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void ruchGracza() {
+        displayPlansza(plansza());
+
+        String czarny = "Y";
+        String bialy = "X";
+
+        String ruch=czarny;
+        String [] staraPlansza=plansza();
+        while (true) {
+            System.out.println("Podaj numer Pola na który chcesz sie przesunac");
+            Scanner in = new Scanner(System.in);
+            String s = in.nextLine();
+
+            String[] plansza = staraPlansza;
+            plansza[Integer.parseInt(s)] = ruch;
+            ruch=bialy;
+
+            displayPlansza(plansza);
+
+            staraPlansza=plansza;
         }
     }
 }
