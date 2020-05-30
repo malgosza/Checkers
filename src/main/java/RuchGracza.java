ddc11d099ac458a75476420a12ca6c467c747354
Gosia <malgosza.niewiadomska@gmail.com>
2020-05-30 14:24:11 +0200

    Adding przesuniecieFiguryNaWybranePole

--- src/main/java/RuchGracza.java
+++ src/main/java/RuchGracza.java
@@ -6,23 +6,22 @@ public class RuchGracza {
     public RuchGracza() {
     }
 
-    public List<Integer> ruszanieSiePoPolachGdzieMozna(int indeksPolaZktoregoRuszam, Plansza plansza){
+    public List<Integer> ruszanieSiePoPolachGdzieMozna(int indeksPolaZktoregoRuszam, Plansza plansza) {
 
         List<Integer> list = new ArrayList<>();
         List<Integer> out = new ArrayList<>();
 
-        list.add(indeksPolaZktoregoRuszam-9);
-        list.add(indeksPolaZktoregoRuszam-7);
-        list.add(indeksPolaZktoregoRuszam+7);
-        list.add(indeksPolaZktoregoRuszam+9);
+        list.add(indeksPolaZktoregoRuszam - 9);
+        list.add(indeksPolaZktoregoRuszam - 7);
+        list.add(indeksPolaZktoregoRuszam + 7);
+        list.add(indeksPolaZktoregoRuszam + 9);
 
-        List<Integer> doUsuniecia=new ArrayList<>();
+        List<Integer> doUsuniecia = new ArrayList<>();
 
         for (int i = 0; i < list.size(); i++) {
-            if(list.get(i)< 0 || list.get(i)>63){
+            if (list.get(i) < 0 || list.get(i) > 63) {
                 doUsuniecia.add(list.get(i));
-            }
-            else if (plansza.getColor(list.get(i)).equals(KolorPola.bialy)){
+            } else if (plansza.getColor(list.get(i)).equals(KolorPola.bialy)) {
                 doUsuniecia.add(list.get(i));
             }
         }
@@ -32,10 +31,22 @@ public class RuchGracza {
         }
 
         for (int i = 0; i < list.size(); i++) {
-            if (plansza.getZawartoscPola(list.get(i)).equals(ZawartoscPola.pusty)){
+            if (plansza.getZawartoscPola(list.get(i)).equals(ZawartoscPola.pusty) ||
+                    plansza.getColor(list.get(i)).equals(KolorPola.czarny)) {
                 out.add(list.get(i));
             }
         }
+
+        System.out.println("Pola po ktorych mozesz sie poruszac: ");
+        for (int i = 0; i < out.size(); i++) {
+            System.out.print(out.get(i) + " ");
+        }
+        System.out.println();
+
         return out;
+    }
+
+    public void przesuniecieFiguryNaWybranePole(int indeksPolaNaKtoreRuszam, Plansza plansza, ZawartoscPola zawartoscPola) {
+        plansza.setZawartoscPola(indeksPolaNaKtoreRuszam, zawartoscPola);
     }
 }
--- src/main/java/StartApp.java
+++ src/main/java/StartApp.java
@@ -1,18 +1,32 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class StartApp {
     public static void main(String[] args) {
         Plansza plansza=new Plansza();
         plansza.ustawianieZawartosciPlanszy();
         displayPlansza(plansza);
         RuchGracza ruch=new RuchGracza();
-        ruch.ruszanieSiePoPolachGdzieMozna(63,plansza);
 
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Numer pola którym chcesz sie poruszyc?");
-//        String numerPolaDoZmiany = scan.nextLine();
-//
-//        Scanner scan2 = new Scanner(System.in);
-//        System.out.println("Numer pola na które chcesz sie poruszyc?");
-//        String numerPolaNaZmiane = scan2.nextLine();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Numer pola którym chcesz sie poruszyc?");
+        String numerPolaKtorymRuszamPionek = scan.nextLine();
+        while (plansza.getColor(Integer.parseInt(numerPolaKtorymRuszamPionek)).equals(KolorPola.bialy)){
+            System.out.println("Musisz poruszac sie TYLKO po czarnych! \nNumer pola którym chcesz sie poruszyc?");
+            numerPolaKtorymRuszamPionek = scan.nextLine();
+        }
+        List<Integer> dostepnePola=ruch.ruszanieSiePoPolachGdzieMozna(Integer.parseInt(numerPolaKtorymRuszamPionek),plansza);
+
+
+        System.out.println("Numer pola na które chcesz sie poruszyc?");
+        Scanner scan2 = new Scanner(System.in);
+        String numerPolaNaKtorePrzesuwamFigure = scan2.nextLine();
+        while (!dostepnePola.contains(Integer.parseInt(numerPolaNaKtorePrzesuwamFigure))){
+            System.out.println("Niezgodnie z zasadami! \nNumer pola na które chcesz sie poruszyc?");
+            scan2 = new Scanner(System.in);
+            numerPolaNaKtorePrzesuwamFigure = scan2.nextLine();
+        }
+        ruch.przesuniecieFiguryNaWybranePole(Integer.parseInt(numerPolaNaKtorePrzesuwamFigure),plansza,ZawartoscPola.iks);
     }
 
     public static void displayPlansza(Plansza plansza) {
@@ -20,14 +34,9 @@ public class StartApp {
             if (i % 8 == 0 && i != 0) {
                 System.out.println();
             }
-            System.out.print(plansza.getColor(i) + " ");
+            System.out.print(plansza.getColor(i) + "("+plansza.getIndexPlansza(i)+") ");
         }
         System.out.println();
     }
-    // logika ->
-    // numer pola ktory chce ruszyc
-    // numer pola na który chce pzresunac pionek
-    //sprawdzam zawartosc
-    //decyzja czy moge czu nie moge
 
 }
